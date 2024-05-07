fd8bd8637024292727bd8bbc12344da2819fb644
Vinicius.Viana <viniciusvianav1099@gmail.com>
2024-05-06 21:30:35 -0300

    dando uns tapinhas no trabalho

---
+++ tela_gerente/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#btnBanco').addEventListener('click', function(){
+    window.location.href = '../tela_bancoHoras/index.html';
+})
+document.querySelector('#btnFolga').addEventListener('click', function(){
+    window.location.href = '../tela_folgaFuncionarios/index.html';
+})
+document.querySelector('#btnExtra').addEventListener('click', function(){
+    window.location.href = '../tela_extraFuncionarios/index.html';
+})
