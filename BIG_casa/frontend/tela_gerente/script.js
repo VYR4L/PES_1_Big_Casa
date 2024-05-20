b600c801fb56ec1393728480356f24f1478e6415
Vinicius.Viana <viniciusvianav1099@gmail.com>
2024-05-19 23:09:01 -0300

    Adicionei a nossa tela_gerente o slide das opções de gerente

--- BIG_casa/frontend/tela_gerente/script.js
+++ BIG_casa/frontend/tela_gerente/script.js
@@ -10,3 +10,16 @@ document.querySelector('#btnExtra').addEventListener('click', function(){
 document.querySelector('#shutButton').addEventListener('click', function(){
     window.location.href = '../tela_login/index.html';
 })
+let state = 0
+function slide(){
+    let slide = document.querySelector("footer")
+    if(state == 0){
+        console.log("porra")
+        slide.style.bottom = "0"
+        state = 1
+    }else{
+        console.log("pika")
+        slide.style.bottom = "-85vh"
+        state = 0
+    }
+}
