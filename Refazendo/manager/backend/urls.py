bb8c16d17819679666799c085006bbeab892dbbe
VYR4L <fkz.zanatt@gmail.com>
2024-06-02 20:08:20 -0300

    Update das URLS

--- Refazendo/manager/backend/urls.py
+++ Refazendo/manager/backend/urls.py
@@ -6,4 +6,6 @@ app_name = "manager.backend"
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login_user'),
     path('annual-leave/', views.UserAnnualLeave.as_view(), name='user_annual_leave'),
+    path('extra-hours/', views.UserExtraHours.as_view(), name='user_extra_hours'),
+    path('day-off/', views.UserDayOff.as_view(), name='user_day_off'),
 ]
--- Refazendo/manager/urls.py
+++ Refazendo/manager/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('manage-user/<int:pk>/', views.ManageUsers.as_view(), name='manage_user'),
     path('users-annual-leave/', views.ManagerAnnualLeave.as_view(), name='users_annual_leave'),
     path('user-annual-leave/<int:pk>/', views.ManagerAnnualLeave.as_view(), name='user_annual_leave'),
+    path('users-extra-hours/', views.ManagerExtraHours.as_view(), name='users_extra_hours'),
+    path('user-extra-hours/<int:pk>/', views.ManagerExtraHours.as_view(), name='user_extra_hours'),
+    path('users-day-off/', views.ManagerDayOff.as_view(), name='users_day_off'),
+    path('user-day-off/<int:pk>/', views.ManagerDayOff.as_view(), name='user_day_off'),
 ]
